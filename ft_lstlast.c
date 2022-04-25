f044d3fb92d0dc94907c71f44699cb4f0845dd52
Philipp Nolte <pnolte@1-C-11.42heilbronn.de>
2022-04-25 16:31:06 +0200

    libft

---
+++ ft_lstlast.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/11 14:13:21 by pnolte            #+#    #+#             */
+/*   Updated: 2022/04/13 11:35:12 by pnolte           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst == NULL)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
